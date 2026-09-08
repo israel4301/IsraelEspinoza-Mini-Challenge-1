2f65cebd73c93f0a8ac59bcd1aae6415bd6b86aa
Israel4301 <israelespinoza159@gmail.com>
2026-09-08 15:49:44 -0700

    First Commit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿//Israel Espinoza
+//Peer Review Name
+//Mini Challenge hello
+//I used a variable to save a user name.
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Israel Espinoza");
+Console.WriteLine("Hello, Israel Espinoza");
+String firstname = "Israel";
